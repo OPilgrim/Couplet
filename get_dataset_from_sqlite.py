5e63950bae0ea0a6079d95f073b83d944bfd0f3b
OPilgrim <835448816@qq.com>
2020-11-11 22:33:07 +0800

    waiting for mysql table

--- get_dataset_from_sqlite.py
+++ get_dataset_from_sqlite.py
@@ -1,25 +1,38 @@
-# 获取数据集
-import sqlite3
+import pymysql
+import traceback
 import os
-couplet_path = '/root/Couplet/couplet/train/'
-'''
-conn = sqlite3.connect('')   # 请输入数据库文件
-cursor = conn.cursor()
+import logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
+logger.info(" connect with Mysql......")
+db = pymysql.connect(host='139.9.113.51', port=3306, user='chatbot', password='chatbot', db='chatbot', charset='utf8')
+cursor = db.cursor()
+
+try:
+    cursor.execute('SELECT * from Corpus')
+    result = cursor.fetchall()
+except:
+    info = sys.exc_info()
+    logger.info(info[0], ':', info[1])
+    db.rollback()
+finally:
+    cursor.close()
+    conn.close()
+logger.info(" close Mysql.")
+'''
+couplet_path = '/root/Couplet/couplet/temp/'
 couplet_ups = list()
 couplet_downs = list()
 
-cursor.execute('select * from Couplet_warehouse')
-for value in cursor.fetchall():
-    couplet_up.append(' '.join([i for i in value[1]])+'\n')
-    couplet_down.append(' '.join([i for i in value[2]])+'\n')
-
-cursor.close()
-conn.close()
+for value in result:
+    couplet_ups.append(' '.join([i for i in value[1]])+'\n')
+    couplet_downs.append(' '.join([i for i in value[2]])+'\n')
 
 with open(os.path.join(couplet_path, 'in.txt'), 'w') as f:
     f.writelines(couplet_ups)
 with open(os.path.join(couplet_path, 'out.txt'), 'w') as f:
     f.writelines(couplet_downs)
 '''
-print("\nget train data from Mysql.\n")
+logger.info(" save train data in {}.".format(couplet_path))
